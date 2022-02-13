0d0a9fa9a119f1d6ae5c8ffcc0c381aa1ca8d391
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-13 11:28:12 -0300

    Remove side panel

--- src/containers/HomePage/HomePage.js
+++ src/containers/HomePage/HomePage.js
@@ -1,15 +1,11 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
-import SidePanel from '../../components/SidePanel/SidePanel';
 import '../../styles/HomePage.css';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div className="HomePage">
-        <div>
-          <SidePanel />
-        </div>
         <div>
           <h1>Lorem ipsum dolor sit amet, consectetur adipiscing eliorem ipsum dolor sit amet.</h1>
         </div>
