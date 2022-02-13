36fee4240487ba0bd662a9133b601f98d16199eb
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-13 16:28:13 -0300

    Finish header basic styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,20 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import '../../styles/Header.css';
+import PixelBall from '../../assets/images/pokeball.png';
 
 class Header extends React.Component {
   render() {
     return (
       <div className="Header">
-        <h1>Pokedex</h1>
+        <div className="header-logo">
+          <h1>Pokedex</h1>
+          <img src={PixelBall} alt="pixel pokeball" />
+        </div>
         <div className="Header-links">
           <a href="/">Pokemons</a>
           <a href="/">Types</a>
-          <a href="/">Something</a>
+          <a href="/">Generation</a>
         </div>
       </div>
     );
