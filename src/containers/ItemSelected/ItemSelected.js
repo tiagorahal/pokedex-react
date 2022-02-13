8ec146003051c88959eb0df727f38d1410f462d7
Tiago Rahal Aires <rahal.aires@gmail.com>
2022-02-13 18:32:47 -0300

    Change font of mid section pokemon info and fix positioning

--- src/containers/ItemSelected/ItemSelected.js
+++ src/containers/ItemSelected/ItemSelected.js
@@ -3,6 +3,15 @@ import React from 'react';
 import TopImage from '../../assets/images/pokelogo.png';
 import '../../styles/ItemSelected.css';
 
+const pokemon = {
+  name: 'poke name',
+  id: 'poke id',
+  type: 'poke type',
+  height: 'poke height',
+  weight: 'poke weight',
+  generation: 'poke gen',
+};
+
 class ItemSelected extends React.Component {
   render() {
     return (
@@ -10,14 +19,32 @@ class ItemSelected extends React.Component {
         <div className="TopSection">
           <figure className="TopImage">
             <img src={TopImage} alt="Pokemon" />
-            <figcaption>Pokemon Name / id</figcaption>
+            <figcaption>
+              Name:&#160;
+              {pokemon.name}
+              <br />
+              Id:&#160;
+              {pokemon.id}
+            </figcaption>
           </figure>
         </div>
         <div className="MidSection">
-          <p>type: ----</p>
-          <p>height: ----</p>
-          <p>weight: ----</p>
-          <p>generation: ----</p>
+          <p>
+            Type:&#160;
+            {pokemon.type}
+          </p>
+          <p>
+            Height:&#160;
+            {pokemon.height}
+          </p>
+          <p>
+            Weight:&#160;
+            {pokemon.weight}
+          </p>
+          <p>
+            Generation:&#160;
+            {pokemon.generation}
+          </p>
         </div>
       </div>
     );
